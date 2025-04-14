1596623be20d643e5d5d66e728fa368cb33670b0
Gustavo Ponce <52498029+gfp2416@users.noreply.github.com>
2025-04-14 16:53:40 -0300

    actualización de estilos

--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using GustavoPonceLanding_CVCompleto; // Cambia esto si tu proyecto usa otro namespace
+using GustavoPonceLanding_CVCompleto;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
